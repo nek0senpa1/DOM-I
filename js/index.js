4902e250b4afbc94c5cb6f22a27c4f08f3a7b0e7
amberm <nekosenpai@gmail.com>
2018-12-03 18:25:19 -0700

    done with the primary thingies

--- js/index.js
+++ js/index.js
@@ -47,6 +47,12 @@ logo2.src = "img/header-img.png";
 const logo3 = document.getElementById("middle-img");
 logo3.src = "img/mid-page-accent.jpg";
 
+const botto = document.querySelector('.contact h4');
+botto.textContent = siteContent.contact["contact-h4"];
+
+const foots = document.querySelector('footer p');
+foots.textContent = siteContent.footer.copyright;
+
 const ctathing = document.querySelector('.cta-text h1');
 ctathing.textContent = siteContent.cta.h1;
 
@@ -67,13 +73,33 @@ const middleThing = document.querySelectorAll('.text-content p');
 
 let widget2 = Array.from(middleThing);
 
-
 widget2[0].textContent = siteContent["main-content"]["features-content"];
 widget2[1].textContent = siteContent["main-content"]["about-content"];
 widget2[2].textContent = siteContent["main-content"]["services-content"];
 widget2[3].textContent = siteContent["main-content"]["product-content"];
 widget2[4].textContent = siteContent["main-content"]["vision-content"];
 
+let widget3 = [];
+
+let navs = document.querySelectorAll("nav a");
+for (i =0; i < navs.length; i++){
+  widget3.push(navs[i])
+}
+
+widget3[0].textContent = siteContent.nav["nav-item-1"];
+widget3[1].textContent = siteContent.nav["nav-item-2"];
+widget3[2].textContent = siteContent.nav["nav-item-3"];
+widget3[3].textContent = siteContent.nav["nav-item-4"];
+widget3[4].textContent = siteContent.nav["nav-item-5"];
+widget3[5].textContent = siteContent.nav["nav-item-6"];
+
+const conctactthing = document.querySelectorAll('.contact p');
+
+let widget4 = Array.from(conctactthing);
+
+widget4[0].textContent = siteContent.contact.address;
+widget4[1].textContent = siteContent.contact.email;
+widget4[2].textContent = siteContent.contact.phone;
 
 
 
